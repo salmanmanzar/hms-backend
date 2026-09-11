8c307fc87789f1252e9ca18f78c9c7942a8da597
Muhammad Salman Manzar <114851256+salmanmanzar@users.noreply.github.com>
2026-09-11 17:01:03 +0500

    Fix Production Cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3001',
+      'https://hms-frontend-gamma-six.vercel.app',
+    ],
     credentials: true,
   });
 
